a62bf6f8a05a25151a911e89bcb2da3773f55654
vishal kajjam <vishal.kvn@gmail.com>
2012-08-23 08:51:13 -0400

    added unit tests

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Location do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe 'should return weather' do
+	    it 'should return weather for valid latitude and longitude' do
+	      # Location.should_receive(:weather).with()
+	      Location.weather(40.0409, -75.6374)
+	    end
+	    it 'should raise Error for invalid latitude and longitude' do
+	    	lambda {Location.weather("zxczxc", "sdfsd")}.should raise_error("something is wrong")
+	    end
+	end
 end
