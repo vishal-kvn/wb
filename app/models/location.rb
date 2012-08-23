c5a0974b03bfbff6e2aedcedf6fdf63e05e07780
vishal kajjam <vishal.kvn@gmail.com>
2012-08-23 08:53:27 -0400

    added code for the location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,15 @@
-class Location < ActiveRecord::Base
+# class Location < ActiveRecord::Base
+class Location < Hashie::Mash
+	def self.weather(lat, lng)
+		begin
+			@url = "http://api.met.no/weatherapi/locationforecast/1.8/?lat=#{lat};lon=#{lng}"
+			@response = RestClient.get @url
+			@response_hash = Hash.from_xml(@response)
+			@response_hash["weatherdata"]["product"]["time"][0]["location"]
+			#add error check when api is down
+		rescue 
+			# raise(RestClient::BadRequest, "test erroring")
+			raise("something is wrong")
+		end
+	end
 end
